ff097b74821e2d9cf923f5233c6cb08fa656abd0
Gilmar Silva Santos Junior <gilmar.sisjunior@gmail.com>
2021-06-14 21:49:27 -0300

    Relacionamentos e DB criados

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use app\Models\Customer;
 
 class User extends Authenticatable
 {
@@ -40,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function User()
+    {
+      return  $this->hasMany(Customer::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/a', function () {
+Route::get('/', function () {
     return view('welcome');
 });
