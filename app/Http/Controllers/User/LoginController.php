f5c5f7c8a7b1d839c959f794e5865a277541ac4a
Gilmar Silva Santos Junior <gilmar.sisjunior@gmail.com>
2021-06-17 02:21:32 -0300

    Login implementado

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers\User;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 
 class LoginController extends Controller
 {
     public function authenticate(Request $request)
-    {
-        if(Auth::attempt(['email'=>$request->email,'password'=>Hash::check($request->password)])){
-
-            return 'working';
+    {   
+        if(!Auth::attempt(['email'=>$request->email,'password'=>$request->password]))
+        {
+            throw new Exception('Favor, confira suas credênciais');
+            return 0;
         }
+        return redirect('/');
     }
 }
